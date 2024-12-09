32acfe6f24bd926ff833836bfd1aa2ac0153cae6
Thiago802 <105284314+Thiago802@users.noreply.github.com>
2024-12-09 20:53:12 -0300

    Update main.c
    criação dos CRUD
    TRIPULAÇÃO
    VOO
    ASSENTO

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 #define MAX_PASSAGEIROS 20
 #define MAX_TRIPULANTE 20
 #define MAX_VOO 20
+#define MAX_ASSENTO 20
 //contador universal
-int contP=0,contT=0,contVoo=0;
+int contP=0,contT=0,contVoo=0, contAss=0;
 
 struct Data{
     int dia;
@@ -66,73 +67,143 @@ struct Reserva{
     struct Assento AssentoReservado;
     struct Passageiro CodPassageiro;
 };
+//CRUD TRIPULAÇÃO
 
+//CREAT
 void PreencheTripulacao(struct Tripulacao tripulantes[MAX_TRIPULANTE]){
     printf("cod tripulante: ");
-    scanf("%i", tripulantes[contT].cod_tripulante);
+    scanf("%i", &tripulantes[contT].cod_tripulante);
     fflush(stdin);
 
     printf("Nome: ");
-    scanf(" %[^\n]", tripulantes[contT].nomeTripulante);
+    scanf(" %[^\n]", &tripulantes[contT].nomeTripulante);
     fflush(stdin);
 
     printf("Telefone: ");
-    scanf(" %[^\n]", tripulantes[contT].TelTripulante);
+    scanf(" %[^\n]", &tripulantes[contT].TelTripulante);
     fflush(stdin);
 
     printf("Cargo(piloto/copiloto/comissario): ");
-    scanf(" %[^\n]", tripulantes[contT].cargo);
+    scanf(" %[^\n]", &tripulantes[contT].cargo);
     fflush(stdin);
 
     contT++;
 }
+//READ
+void ImprimeTripulacao(struct Tripulacao tripulantes[MAX_TRIPULANTE]){
+        if(contT==0){
+        printf("Nao ha voo");
+        return;
+    }
+
+    for(int i=0;i<contT;i++){
+        printf("\nCódigo de tripulante: %i\n",tripulantes[i].cod_tripulante);
+        printf("Nome do tripulante: %s\n",tripulantes[i].nomeTripulante);
+        printf("Telefone do tripulante: %s\n",tripulantes[i].TelTripulante);
+        printf("Cargo: %s\n", tripulantes[i].cargo);
+    }
+}
+
+//UPDATE
+void UpdateTripulacao(struct Tripulacao tripulantes[MAX_TRIPULANTE]){
+    int cod, val_i;
+    bool verifica=false;
+    printf("Digite o codigo do voo: ");
+    scanf("%i",&cod);
+
+    for(int i=0;i<contT;i++){
+        if(tripulantes[i].cod_tripulante==cod){
+            verifica=true;
+            val_i=i;
+            i=contT;
+        }
+    }
+
+    if(verifica == true){
+        printf("Nome: ");
+        scanf(" %[^\n]", &tripulantes[contT].nomeTripulante);
+        fflush(stdin);
+
+        printf("Telefone: ");
+        scanf(" %[^\n]", &tripulantes[contT].TelTripulante);
+        fflush(stdin);
+
+        printf("Cargo(piloto/copiloto/comissario): ");
+        scanf(" %[^\n]", &tripulantes[contT].cargo);
+        fflush(stdin);
+    }
+}
+
+//DELETE
+void DeleteTripulacao(struct Tripulacao tripulantes[MAX_TRIPULANTE]){
+        int cod, val_i;
+    bool verifica=false;
+    printf("Digite codigo de tripulante que sera deletado: ");
+    scanf("%i", &cod);
+
+    for(int i=0;i<contT;i++){
+        if(tripulantes[i].cod_tripulante==cod){
+            verifica=true;
+            val_i=i;
+        }
+    }
+    if(verifica==true){
+
+        for(int i=val_i-1;i<MAX_TRIPULANTE-1;i++){
+            tripulantes[i] = tripulantes[i++];
+        }
+        contT-1;
+    }
+}
+//FIM CRUD TRIPULAÇÃO
+
 //CRUD VOO
 
 //CREAT
-void PreencheVoo(struct Voo voo){
+void PreencheVoo(struct Voo voo[MAX_VOO]){
     if(contVoo < MAX_VOO){
         printf("cod voo: ");
-        scanf("%i",&voo.cod_voo);
+        scanf("%i",&voo[contVoo].cod_voo);
         fflush(stdin);
 
         printf("data dd/mm/aaaa: ");
-        scanf("%d/%d/%d",&voo.DataVoo.dia,&voo.DataVoo.mes,&voo.DataVoo.ano);
+        scanf("%d/%d/%d",&voo[contVoo].DataVoo.dia,&voo[contVoo].DataVoo.mes,&voo[contVoo].DataVoo.ano);
         fflush(stdin);
 
         printf("Hora hh:mm : ");
-        scanf("%d:%d",&voo.HoraVoo.hora,&voo.HoraVoo.minuto);
+        scanf("%d:%d",&voo[contVoo].HoraVoo.hora,&voo[contVoo].HoraVoo.minuto);
         fflush(stdin);
 
         printf("Origem do voo: ");
-        scanf(" %[^\n]", &voo.origem);
+        scanf(" %[^\n]", &voo[contVoo].origem);
         fflush(stdin);
 
         printf("Destino do voo: ");
-        scanf(" %[^\n]", &voo.destino);
+        scanf(" %[^\n]", &voo[contVoo].destino);
         fflush(stdin);
 
         printf("Codigo do avião: ");
-        scanf("%i", voo.cod_aviao);
+        scanf("%i", voo[contVoo].cod_aviao);
         fflush(stdin);
 
         printf("Codigo do piloto: ");
-        scanf("%i", voo.cod_piloto);
+        scanf("%i", voo[contVoo].cod_piloto);
         fflush(stdin);
 
         printf("Codigo do copiloto: ");
-        scanf("%i", voo.cod_copiloto);
+        scanf("%i", voo[contVoo].cod_copiloto);
         fflush(stdin);
 
         printf("Codigo do/a comissario/a de bordo: ");
-        scanf("%i", voo.cod_comissaria);
+        scanf("%i", voo[contVoo].cod_comissaria);
         fflush(stdin);
 
         printf("STATUS(ativo/inativo): ");
-        scanf(" %[^\n]", voo.status);
+        scanf(" %[^\n]", voo[contVoo].status);
         fflush(stdin);
 
         printf("Tarifa: ");
-        scanf("%.2f", voo.tarifa);
+        scanf("%.2f", voo[contVoo].tarifa);
         fflush(stdin);
 
 
@@ -180,11 +251,74 @@ void UpdateVoo(struct Voo voo[MAX_VOO]){
     }
     if(verifica==true){
 
+        printf("data dd/mm/aaaa: ");
+        scanf("%d/%d/%d",&voo[contVoo].DataVoo.dia,&voo[contVoo].DataVoo.mes,&voo[contVoo].DataVoo.ano);
+        fflush(stdin);
+
+        printf("Hora hh:mm : ");
+        scanf("%d:%d",&voo[contVoo].HoraVoo.hora,&voo[contVoo].HoraVoo.minuto);
+        fflush(stdin);
+
+        printf("Origem do voo: ");
+        scanf(" %[^\n]", &voo[contVoo].origem);
+        fflush(stdin);
+
+        printf("Destino do voo: ");
+        scanf(" %[^\n]", &voo[contVoo].destino);
+        fflush(stdin);
+
+        printf("Codigo do avião: ");
+        scanf("%i", voo[contVoo].cod_aviao);
+        fflush(stdin);
+
+        printf("Codigo do piloto: ");
+        scanf("%i", voo[contVoo].cod_piloto);
+        fflush(stdin);
+
+        printf("Codigo do copiloto: ");
+        scanf("%i", voo[contVoo].cod_copiloto);
+        fflush(stdin);
+
+        printf("Codigo do/a comissario/a de bordo: ");
+        scanf("%i", voo[contVoo].cod_comissaria);
+        fflush(stdin);
+
+        printf("STATUS(ativo/inativo): ");
+        scanf(" %[^\n]", voo[contVoo].status);
+        fflush(stdin);
+
+        printf("Tarifa: ");
+        scanf("%.2f", voo[contVoo].tarifa);
+        fflush(stdin);
+
+
+
     }
 
 }
 
+//DELETE
+void DeleteVoo(struct Voo voo[MAX_VOO]){
+    int cod, val_i;
+    bool verifica=false;
+    printf("Digite codigo de VOO que sera deletado: ");
+    scanf("%i", &cod);
 
+    for(int i=0;i<contT;i++){
+        if(voo[i].cod_voo==cod){
+            verifica=true;
+            val_i=i;
+        }
+    }
+    if(verifica == true){
+        for(int i=val_i-1;i<MAX_VOO-1;i++){
+            voo[i] = voo[i++];
+        }
+        contVoo-1;
+
+    }
+}
+//FIM CRUD VOO
 
 //CRUD PASSAGEIRO
 
@@ -228,7 +362,7 @@ void PreenchePassageiro(struct Passageiro passageiros[MAX_PASSAGEIROS]){
         contP++;
     }
     else
-        printf("ja nao ah assentos");
+        printf("já não ah assentos");
 
 }
 
@@ -302,19 +436,89 @@ void DeletePassageiro(struct Passageiro passageiros[MAX_PASSAGEIROS]){
 }
 //FIM DE CRUD PASSAGEIRO
 
-void PreencheAssento(struct Assento assento){
+//INICIO CRUD ASSENTO
+
+//CREAT
+void PreencheAssento(struct Assento assento[MAX_ASSENTO]){
     printf("Numero do assento: ");
-    scanf("%i", &assento.num_assento);
+    scanf("%i", &assento[contAss].num_assento);
     fflush(stdin);
 
     printf("codigo do voo: ");
-    scanf("%i", &assento.CodVoo.cod_voo);
+    scanf("%i", &assento[contAss].CodVoo.cod_voo);
     fflush(stdin);
 
     printf("Status da cadeira: ");
-    scanf(" [^\n]", &assento.Status_assento);
+    scanf(" [^\n]", &assento[contAss].Status_assento);
     fflush(stdin);
+
+    contAss++;
 }
+
+//READ
+void ImprimeAssento(struct Assento assento[MAX_ASSENTO]){
+    if(contAss=0){
+        printf("Assento não cadastrado");
+        return;
+    }
+    printf("código do voo: %i\n",assento[contAss].CodVoo.cod_voo);
+    printf("Número do assento: %i\n",assento[contAss].num_assento);
+    printf("STATUS: %s\n",assento[contAss].Status_assento);
+}
+
+//UPDATE
+void UpdateAssento(struct Assento assento[MAX_ASSENTO]){
+    int cod, val_i;
+    bool verifica=false;
+    printf("Digite codigo de passageiro: ");
+    scanf("%i", &cod);
+
+    for(int i=0;i<contP;i++){
+        if(assento[i].CodVoo.cod_voo==cod){
+            verifica=true;
+            val_i=i;
+            i=contAss;
+        }
+    }
+    if(verifica == true){
+        printf("Status da cadeira: ");
+        scanf(" [^\n]", &assento[contAss].Status_assento);
+        fflush(stdin);
+
+        printf("Numero do assento: ");
+        scanf("%i", &assento[contAss].num_assento);
+        fflush(stdin);
+    }
+
+}
+
+//DELETE
+void DeleteAssento(struct Assento assento[MAX_ASSENTO]){
+    int cod, val_i;
+    bool verifica=false;
+    printf("Digite código de VOO que será deletado: ");
+    scanf("%i", &cod);
+
+    for(int i=0;i<contAss;i++){
+        if(assento[i].CodVoo.cod_voo==cod){
+            verifica=true;
+            val_i=i;
+        }
+    }
+    if(verifica==true){
+
+        for(int i=val_i-1;i<MAX_ASSENTO-1;i++){
+            assento[i] = assento[i++];
+        }
+        contAss-1;
+
+
+    }
+}
+//FIM CRUD ASSENTO
+
+
+
 //inicio backup
 void pegaDados(const char nomearquivo,struct Passageiro passageiros,int total){
     FILE *arq;
@@ -339,7 +543,7 @@ void bkup(struct Passageiro passageiros){
 
 int main()
 {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+    setlocale(LC_ALL, "Portuguese");
 
     FILE *backupPass;
 
@@ -347,22 +551,105 @@ int main()
     FILE *pass;
     FILE *assent;
     FILE *res;
+    int caso,subcaso_passageiro,subcaso_tripulacao,subclasse_voo,subclasse_Assento;
 
     struct Tripulacao tripulantes[MAX_TRIPULANTE];
     struct Passageiro passageiros[MAX_PASSAGEIROS];
-    struct Voo voo;
-    struct Assento assento;
+    struct Voo voo[MAX_VOO];
+    struct Assento assento[MAX_ASSENTO];
     struct Reserva reserva;
+    printf("----MENU----\n");
+    printf("1-Passageiro\n2-Tripulação\n3-Voo\n4-Assento\n5-Reserva\n");
+    printf("---- ---- ----");
 
+    switch (caso){
+    case 1:
+        printf("\n---- PASSAGEIRO ----\n");
+        printf("1-Preencher\n2-Atualizar\n3-Deletar passageiro\n4-Imprimir informações");
+        printf("\n---- ---- ----");
+        switch(subcaso_passageiro){
+        case 1:
+            PreenchePassageiro(passageiros);
+            break;
 
+        case 2:
+            UpdatePassageiro(passageiros);
+            break;
+
+        case 3:
+            DeletePassageiro(passageiros);
+            break;
+
+        case 4:
+            ImprimePassageiro(passageiros);
+            break;
+        }
+    case 2:
+        printf("\n---- TRIPULAÇÃO ----\n");
+        printf("1-Preencher\n2-Atualizar\n3-Deletar passageiro\n4-Imprimir informações");
+        printf("\n---- ---- ----\n");
+        switch(subcaso_tripulacao){
+        case 1:
+            PreencheTripulacao(tripulantes);
+            break;
+        case 2:
+            UpdateTripulacao(tripulantes);
+            break;
+        case 3:
+            DeleteTripulacao(tripulantes);
+            break;
+        case 4:
+            ImprimeTripulacao(tripulantes);
+            break;
+        }
+    case 3:
+        printf("\n---- VOO ----\n");
+        printf("1-Preencher\n2-Atualizar\n3-Deletar passageiro\n4-Imprimir informações");
+        printf("\n---- ---- ----\n");
+        switch (subclasse_voo){
+        case 1:
+            PreencheVoo(voo);
+            break;
+        case 2:
+            UpdateVoo(voo);
+            break;
+        case 3:
+            DeleteVoo(voo);
+            break;
+        case 4:
+            ImprimeVoo(voo);
+        }
+    case 4:
+        printf("\n---- ASSENTO ----\n");
+        printf("1-Preencher\n2-Atualizar\n3-Deletar passageiro\n4-Imprimir informações");
+        printf("\n---- ---- ----\n");
+        switch(subclasse_Assento){
+        case 1:
+            PreencheAssento(assento);
+            break;
+        case 2:
+            UpdateAssento(assento);
+            break;
+        case 3:
+            DeleteAssento(assento);
+            break;
+        case 4:
+            ImprimeAssento(assento);
+
+        }
+
+    }
     //PreencheVoo(voo);
     //PreenchePassageiro(passageiros);
-   // ImprimePassageiro(passageiros);
+    //ImprimePassageiro(passageiros);
     //UpdatePassageiro(passageiros);
     //DeletePassageiro(passageiros);
     //ImprimePassageiro(passageiros);
-    //PreencheTripulacao(tripulantes);
 
+    //PreencheTripulacao(tripulantes);
+    //ImprimeTripulacao(tripulantes);
+    //UpdateTripulacao(tripulantes);
+    //DeleteVoo(voo);
 
 }
 
@@ -378,4 +665,3 @@ if(!pass){
     fprintf(pass, %i %s %s %s %i,passageiros[i].cod,passageiros[i].nome,passageiros[i].endereco,passageiro[i].tel,passageiros[i].PontoFid);
     }
 */
-
